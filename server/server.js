01610a5fcda9dc90b1b50cbfb5c65d94db75733f
duane11003 <duane.mcfar@gmail.com>
2020-06-06 21:06:47 -0400

    displaying response to console.log on UI

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const config = require("../twit_config/config");
 const cors = require("cors");
-// const path = require("path");
 
 app.use(express.json());
 app.use(cors());
@@ -12,26 +11,15 @@ const Twit = require("twit");
 
 const T = new Twit(config);
 
-// search tweets route
-
 app.post("/search", function (req, res) {
-  console.log(req.body, 'is the req.body')
-  T.get("search/tweets", { q: `%23${req.body}`, count: 1 }, function (
+  T.get("search/tweets", { q: `%23${req.body}`, count: 20 }, function (
     err,
     data,
     response
   ) {
-    
-    res.send(data.statuses)
+    res.send(data.statuses);
   });
-  // res.send("info from twitter");
 });
-
-
-
-
-
-
 
 app.listen(3000, function () {
   console.log("listening on 3000");
--- src/Components/HashtagSearch.js
+++ src/Components/HashtagSearch.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function HashtagSearch() {
-  return <p>from hashtag search component</p>;
+  const [input, setInput] = useState("");
+  const [tweets, setTweets] = useState([]);
+
+  const handleClick = () => {
+    const options = {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-type": "application/json",
+        mode: "no-cors",
+      },
+      body: JSON.stringify({
+        "#": { input },
+      }),
+    };
+    fetch("http://localhost:3000/search", options)
+      .then((res) => res.json())
+      .then((res) => setTweets(res));
+    setInput("");
+    console.log(tweets, "is the updated state");
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="#"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></input>
+      <button onClick={handleClick}>Click</button>
+    </div>
+  );
 }
 
 export default HashtagSearch;
